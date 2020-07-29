21083742d926a8cefbd046e25ba0faeb28dd52d3
Lenon Corrêa <lenonac_@hotmail.com>
2020-07-29 16:00:51 -0300

    Alterado return por print no script da api, tempo limite do zabbix era excedido

--- api_nextcloud.py
+++ api_nextcloud.py
@@ -18,49 +18,48 @@ class Api:
     for result in results:
       if argument == "version":
         version = results['ocs']['data']['nextcloud']['system']['version']
-        return version
+        print(version)
       
       if argument == "freespace":
         freespace = results['ocs']['data']['nextcloud']['system']['freespace']
-        return freespace
+        print(freespace)
       
       if argument == "num_users":
         num_users = results['ocs']['data']['nextcloud']['storage']['num_users']
-        return num_users
+        print(num_users)
       
       if argument == "num_files":
         num_files = results['ocs']['data']['nextcloud']['storage']['num_files']
-        return num_files
+        print(num_files)
       
       if argument == "num_storages_local":
         num_storages_local = results['ocs']['data']['nextcloud']['storage']['num_storages_local']
-        return num_storages_local
+        print(num_storages_local)
       
       if argument == "memory_limit":
         memory_limit = results['ocs']['data']['server']['php']['memory_limit']
-        return memory_limit
+        print(memory_limit)
       
       if argument == "max_execution_time":
         max_execution_time = results['ocs']['data']['server']['php']['max_execution_time']
-        return max_execution_time
+        print(max_execution_time)
       
       if argument == "upload_max_filesize":
         upload_max_filesize = results['ocs']['data']['server']['php']['upload_max_filesize']
-        return upload_max_filesize
+        print(upload_max_filesize)
       
       if argument == "database_size":
         database_size = results['ocs']['data']['server']['database']['size']
-        return database_size
+        print(database_size)
 
       if argument == "activeUsers":
         activeUsers = results['ocs']['data']['activeUsers']['last5minutes']
-        return activeUsers
+        print(activeUsers)
       
 def main(baseUrl,user,token,argument):
   api = Api(baseUrl,user,token)
   results = api.requesting()
   result = api.params(results,argument)
-  return result
 
 if __name__ == '__main__':
   baseUrl = sys.argv[1]
@@ -68,4 +67,3 @@ if __name__ == '__main__':
   token = sys.argv[3]
   argument = sys.argv[4]
   result = main(baseUrl,user,token,argument)
-  print(result)
