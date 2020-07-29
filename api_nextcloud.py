5d57bd0f064389f46490fe70957f3d7007f113f7
Lenon Corrêa <lenonac_@hotmail.com>
2020-07-29 17:32:09 -0300

    Adicionado exit(), diminui o tempo limite

--- api_nextcloud.py
+++ api_nextcloud.py
@@ -19,43 +19,44 @@ class Api:
       if argument == "version":
         version = results['ocs']['data']['nextcloud']['system']['version']
         print(version)
-      
+        exit()
       if argument == "freespace":
         freespace = results['ocs']['data']['nextcloud']['system']['freespace']
         print(freespace)
-      
+        exit()
       if argument == "num_users":
         num_users = results['ocs']['data']['nextcloud']['storage']['num_users']
         print(num_users)
-      
+        exit()
       if argument == "num_files":
         num_files = results['ocs']['data']['nextcloud']['storage']['num_files']
         print(num_files)
-      
+        exit()
       if argument == "num_storages_local":
         num_storages_local = results['ocs']['data']['nextcloud']['storage']['num_storages_local']
         print(num_storages_local)
-      
+        exit()
       if argument == "memory_limit":
         memory_limit = results['ocs']['data']['server']['php']['memory_limit']
         print(memory_limit)
-      
+        exit()
       if argument == "max_execution_time":
         max_execution_time = results['ocs']['data']['server']['php']['max_execution_time']
         print(max_execution_time)
-      
+        exit()
       if argument == "upload_max_filesize":
         upload_max_filesize = results['ocs']['data']['server']['php']['upload_max_filesize']
         print(upload_max_filesize)
-      
+        exit()
       if argument == "database_size":
         database_size = results['ocs']['data']['server']['database']['size']
         print(database_size)
-
+        exit()
       if argument == "activeUsers":
         activeUsers = results['ocs']['data']['activeUsers']['last5minutes']
         print(activeUsers)
-      
+        exit()
+        
 def main(baseUrl,user,token,argument):
   api = Api(baseUrl,user,token)
   results = api.requesting()
