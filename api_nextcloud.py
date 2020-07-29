32be3892d5dcba6ecc590a864e235e00d2917fdc
Lenon Corrêa <lenonac_@hotmail.com>
2020-07-29 19:09:55 -0300

    Voltando ao return em saida do argumento

--- api_nextcloud.py
+++ api_nextcloud.py
@@ -18,49 +18,49 @@ class Api:
     for result in results:
       if argument == "version":
         version = results['ocs']['data']['nextcloud']['system']['version']
-        print(version)
-        exit()
+        return version
+
       if argument == "freespace":
         freespace = results['ocs']['data']['nextcloud']['system']['freespace']
-        print(freespace)
-        exit()
+        return freespace
+
       if argument == "num_users":
         num_users = results['ocs']['data']['nextcloud']['storage']['num_users']
-        print(num_users)
-        exit()
+        return num_users
+
       if argument == "num_files":
         num_files = results['ocs']['data']['nextcloud']['storage']['num_files']
-        print(num_files)
-        exit()
+        return num_files
+
       if argument == "num_storages_local":
         num_storages_local = results['ocs']['data']['nextcloud']['storage']['num_storages_local']
-        print(num_storages_local)
-        exit()
+        return num_storages_local
+
       if argument == "memory_limit":
         memory_limit = results['ocs']['data']['server']['php']['memory_limit']
-        print(memory_limit)
-        exit()
+        return memory_limit
+
       if argument == "max_execution_time":
         max_execution_time = results['ocs']['data']['server']['php']['max_execution_time']
-        print(max_execution_time)
-        exit()
+        return max_execution_time
+
       if argument == "upload_max_filesize":
         upload_max_filesize = results['ocs']['data']['server']['php']['upload_max_filesize']
-        print(upload_max_filesize)
-        exit()
+        return upload_max_filesize
+
       if argument == "database_size":
         database_size = results['ocs']['data']['server']['database']['size']
-        print(database_size)
-        exit()
+        return database_size
+
       if argument == "activeUsers":
         activeUsers = results['ocs']['data']['activeUsers']['last5minutes']
-        print(activeUsers)
-        exit()
+        return activeUsers
         
 def main(baseUrl,user,token,argument):
   api = Api(baseUrl,user,token)
   results = api.requesting()
   result = api.params(results,argument)
+  print(result)
 
 if __name__ == '__main__':
   baseUrl = sys.argv[1]
